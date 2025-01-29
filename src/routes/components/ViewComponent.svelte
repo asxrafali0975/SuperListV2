4d125ec77fd16ee6c087376accfcc2ca508a9e3c
AshrafAli0975 <2023bca113@axiscolleges.in>
2025-01-29 16:25:59 +0530

    Update ViewComponent.svelte

--- src/routes/components/ViewComponent.svelte
+++ src/routes/components/ViewComponent.svelte
@@ -6,7 +6,6 @@
     let edit_task_bool = $state(true);
     import { arr } from "../../store";
 
-    
     const addEditTaskFunc = (index) => {
         if (!editTaskDataBind) {
             alert("task cannot be empty ");
@@ -17,7 +16,6 @@
             edit_task_bool = !edit_task_bool;
         }
     };
-   
 </script>
 
 <button
@@ -25,14 +23,13 @@
     onclick={() => (edit_task_bool = !edit_task_bool)}>Edit</button
 >
 <button
-class={edit_task_bool
+    class={edit_task_bool
         ? "btn btn-outline btn-accent hidden   btn-xs sm:btn-sm md:btn-sm lg:btn-md"
         : "btn btn-outline btn-accent          btn-xs sm:btn-sm md:btn-sm lg:btn-md "}
     onclick={() => addEditTaskFunc(prop.index)}>Add</button
-    >
-    <input
+>
+<input
     type="text"
-    
     placeholder="Enter New Task Here"
     bind:value={editTaskDataBind}
     class={edit_task_bool
@@ -42,9 +39,8 @@ class={edit_task_bool
 
 <style>
     @media (min-width: 280px) and (max-width: 640px) {
-    button {
-        height: 10vw;
+        button {
+            height: 10vw;
+        }
     }
-   
-}
 </style>
